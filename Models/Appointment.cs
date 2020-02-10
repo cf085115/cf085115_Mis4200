ddb19a48faf40b217d1a02e46548706471d1d630
Conor Faut <cf085115@ohio.edu>
2020-02-09 19:00:17 -0500

    gg

---
+++ Models/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cf085115_Mis4200.Models
+{
+    public class Appointment
+    {
+        public int appointmentId { get; set; }
+        public string description { get; set; }
+        public DateTime appointmentDate { get; set; }
+        public ICollection<Patient>Patient { get; set; }
+        public int DoctorId { get; set; }
+        public virtual Doctor Doctor { get; set; }
+    }
+}
---
+++ Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cf085115_Mis4200.Models
+{
+    public class Doctor
+    {
+        public int doctorId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone{ get; set; }
+        public DateTime doctorSince { get; set; }
+        public ICollection<Appointment> Appointment { get; set; }
+    }
+}
